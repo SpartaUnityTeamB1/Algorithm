01def8f5805f4a0d7fcaf0f1bf0421dafed7c608
MilkyQuartz <milkyquartz.2001@gmail.com>
2024-08-29 21:03:01 +0900

    Add : 유수정 16953번 c#

---
+++ 2. Silver/16953/유수정_A → B.cs
@@ -0,0 +1,28 @@
+﻿string[] input = Console.ReadLine().Split();
+int A = int.Parse(input[0]);
+int B = int.Parse(input[1]);
+
+int count = 1;
+while (A != B)
+{
+    if (B < A)
+    {
+        Console.Write(-1);
+        return;
+    }
+    if (B % 10 == 1)
+    {
+        B /= 10;
+    }
+    else if (B % 2 == 0)
+    {
+        B /= 2;
+    }
+    else
+    {
+        Console.Write(-1);
+        return;
+    }
+    count++;
+}
+Console.Write(count);
