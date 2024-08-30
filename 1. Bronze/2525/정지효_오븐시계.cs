711d7940a172cd207e2d15378e4a91735ed6f6dd
jihyo <jgh6213@naver.com>
2024-08-30 21:47:46 +0900

    Add : 정지효 2525 c#
    Fix : 정지효 파일 이동

---
+++ 1. Bronze/2525/정지효_오븐시계.cs
@@ -0,0 +1,22 @@
+﻿string[] input = Console.ReadLine().Split();
+
+int a, b, c;
+
+a = int.Parse(input[0]);
+b = int.Parse(input[1]);
+c = int.Parse(Console.ReadLine());
+
+b += c;
+
+while (b >= 60)
+{
+    a++;
+    b -= 60;
+}
+
+if (a >= 24)
+{
+    a -= 24;
+}
+
+Console.WriteLine(a + " " + b);
