2cce2f8246fffede9aa13283c2f8d239c0ed7fdb
jihyo <jgh6213@naver.com>
2024-08-31 22:19:00 +0900

    Add : 정지효 2480 c#

---
+++ 1. Bronze/2480/2480.cs
@@ -0,0 +1,39 @@
+﻿string[] input = Console.ReadLine().Split();
+
+int a, b, c;
+
+a = int.Parse(input[0]);
+b = int.Parse(input[1]);
+c = int.Parse(input[2]);
+
+if (a == b &&  b == c)
+{
+    Console.WriteLine(10000 + (a * 1000));
+}
+else if (a == b)
+{
+    Console.WriteLine(1000 + a * 100);
+}
+else if (a == c)
+{
+    Console.WriteLine(1000 + a * 100);
+}
+else if (b == c)
+{
+    Console.WriteLine(1000 + b * 100);
+}
+else
+{
+    if (a > b && a > c)
+    {
+        Console.WriteLine(100 * a);
+    }
+    else if (b > c)
+    {
+        Console.WriteLine(100 * b);
+    }
+    else
+    {
+        Console.WriteLine(100 * c);
+    }
+}
