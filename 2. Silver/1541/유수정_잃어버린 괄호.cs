e7dc1274eb78dd01a8501d748b8b5ff2b98ea64b
MilkyQuartz <141620531+MilkyQuartz@users.noreply.github.com>
2024-09-17 21:30:08 +0900

    Add : 유수정 1541 c#

---
+++ 2. Silver/1541/유수정_잃어버린 괄호.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+string n = Console.ReadLine();
+string[] m = n.Split('-');
+
+int answer = 0;
+
+int x = m[0].Split('+').Select(int.Parse).Sum();
+if (n[0] == '-')
+{
+    answer -= x;
+}
+else
+{
+    answer += x;
+}
+
+for (int i = 1; i < m.Length; i++)
+{
+    x = m[i].Split('+').Select(int.Parse).Sum();
+    answer -= x;
+}
+
+Console.Write(answer);
