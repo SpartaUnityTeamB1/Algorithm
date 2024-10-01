fbf2a7d71d9eccde645c55b0a7dda71e5902db99
MilkyQuartz <141620531+MilkyQuartz@users.noreply.github.com>
2024-10-01 16:42:54 +0900

    Add : 유수정 13305번 c#

---
+++ 2. Silver/13305/유수정_주유소.cs
@@ -0,0 +1,21 @@
+﻿int N = Convert.ToInt32(Console.ReadLine());
+long[] d = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+long[] won = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+
+long minWon = won[0];
+long output = 0;
+
+for (int i = 0; i < d.Length; i++)
+{
+    if (minWon < won[i])
+    {
+        output += minWon * d[i];
+    }
+    else
+    {
+        output += won[i] * d[i];
+        minWon = won[i];
+    }
+}
+
+Console.WriteLine(output);
