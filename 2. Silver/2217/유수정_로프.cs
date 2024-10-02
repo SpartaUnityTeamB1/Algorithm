e4a98e6d409d5b588305dfd5324834f7aa831b9d
MilkyQuartz <141620531+MilkyQuartz@users.noreply.github.com>
2024-10-02 17:24:33 +0900

    Add : 유수정 2217번 c#

---
+++ 2. Silver/2217/유수정_로프.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+Array.Sort(array);
+
+int max = 0;
+
+for (int i = 0; i < n; i++)
+{
+    max = array[i] * (n - i) > max ? array[i] * (n - i) : max;
+}
+
+Console.Write(max);
